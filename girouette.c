eeca93ec3984be46ab1f7c4300a8b102335560ed
frggey <frg.julien@gmail.com>
2016-11-10 17:02:54 +0100

    Update girouette.c

--- girouette.c
+++ girouette.c
@@ -5,6 +5,31 @@
 #include "timer1234.h"
 	
 	
+ void init_girouette(TIM_TypeDef*Timer)
+{
+	
+	Timer->ARR=1439;
+	// configuration des deux pin T1 et T2
+	
+	Timer->CCMR1 &=~(0x303);
+	Timer->CCMR1 |= (0x202);
+	
+	// configuration SMCR
+	
+	Timer->SMCR &=~(0x7);
+	Timer->SMCR |=(0x3);
+	
+	// configuration CCER
+	
+	Timer->CCER &=~(0x22);
+	Timer->CCER |=(0x22);
+	
+	//lancer comptage
+	 Timer->CR1 |= TIM_CR1_CEN;
+	
+}
+	
+	
 	
 	
 	
