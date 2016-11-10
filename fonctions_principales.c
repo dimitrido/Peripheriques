95edb930f1b037c302d7c3d1ed67ea813d262d6d
dimitrido <dimitri.doutriaux@hotmail.fr>
2016-11-10 16:53:06 +0100

    Add files via upload

---
+++ fonctions_principales.c
@@ -0,0 +1,10 @@
+
+#include "MoteurCC.h"
+#include "ModuleReception.h"
+
+void Tourner_BT(void){
+ changeDirection(getDutyCycle()*100);
+}
+
+
+
---
+++ fonctions_principales.h
@@ -0,0 +1,5 @@
+
+#include "MoteurCC.h"
+#include "ModuleReception.h"
+
+void Tourner_BT(void);
