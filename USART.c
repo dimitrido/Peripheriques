c92bd7caf1287b365915286acde50867c7a60e72
dimitrido <dimitri.doutriaux@hotmail.fr>
2016-11-10 09:56:38 +0100

    Update USART.c

--- USART.c
+++ USART.c
@@ -2,7 +2,9 @@
 	#include "clock.h"
 	#include "gpio.h"
 
-void Init_Usart_Trans(USART_TypeDef * USART){
+void USART_Init_Trans(USART_TypeDef * USART){
+RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+Port_IO_Init_Output( GPIOA, 9);
 USART->CR1 &= ~(0x1 <<12);
 USART->CR1 |= (0x01<<3);	
 USART->BRR |= (0x1D4<<4);
@@ -11,7 +13,8 @@ USART-> CR2 &= ~(0x3<<12);
 USART->CR1 |= (0x01<<13);
 }
 
-void Init_Usart_Recep(USART_TypeDef *USART){
+void USART_Init_Recep(USART_TypeDef *USART){
+RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
 USART->CR1 &= ~(0x1 <<12);
 USART->CR1 |= (0x01<<2);	
 USART->BRR |= (0x1D4<<4);
@@ -23,7 +26,7 @@ USART->CR1 |= (0x01<<13);
 
 
 int USART_Send (USART_TypeDef * USART, u8 data){
-if ((USART->CR2 & (0x01<<7))==1){
+if ((USART->DR )==0){ //tester avec TXE
 	USART->DR = data;
 	return 0;
 }
