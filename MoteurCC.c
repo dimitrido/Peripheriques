9a854440cf8c4153c4cf8bc69caba793ce84de38
dimitrido <dimitri.doutriaux@hotmail.fr>
2016-11-17 09:59:55 +0100

    Update MoteurCC.c

--- MoteurCC.c
+++ MoteurCC.c
@@ -3,20 +3,20 @@
 	#include "gpio.h"
 	#include "Timer1234.h"
 
-void MoteurCC_Init(float freqTelecommande){ //dépend de la télécommande
-	Port_IO_Init_Output( GPIOA, 2, 0);
-	PWM_init(TIM2, '3', freqTelecommande);
+void MoteurCC_Init(float freqTelecommande){ //dÃ©pend de la tÃ©lÃ©commande
+	//Port_IO_Init_Output( GPIOA, 2, 0);
+	PWM_init(TIM2, 3, freqTelecommande);
 	Set_duty_cycle(TIM2, '3', 0);
 }
 
 void changeDirection(float dutyCycle){
 	if (dutyCycle < 7.5){
 	Port_IO_Set(GPIOA,2);
-	Set_duty_cycle(TIM2,'3',0.5);
+	Set_duty_cycle(TIM2,3,25);
 	}
 	else if (dutyCycle > 7.5) {
 	Port_IO_Reset(GPIOA,2);
-	Set_duty_cycle(TIM2,'3',0.5);
+	Set_duty_cycle(TIM2,3,25);
 	}
 	else {Set_duty_cycle(TIM2,'3',0);}
 }
