86e53f9ce4635912a7b5a78cb64d2ea3ad0202b3
frggey <frg.julien@gmail.com>
2016-11-22 09:07:22 +0100

    Update servomoteur.c

--- servomoteur.c
+++ servomoteur.c
@@ -3,38 +3,20 @@
 #include "clock.h"
 #include "gpio.h"	
 #include "timer1234.h"
+#include "girouette.h"
 	
 	
 	
-	
-	
-float pos_servomoteur(int angle, int anglemax, int anglemin,TIM_TypeDef*Timer, char Voie )  // sortie sur GPIOB.8 pour TIM4_CH3
-{	
-	static float pwm ;
-	static int delta_angle;
-	
-	delta_angle= anglemax-anglemin;
+void init_servo(TIM_TypeDef*Timer, char Voie)
+{
 	PWM_init(Timer, Voie, 0.050);
+	Set_duty_cycle(Timer, Voie , 10);
 	
-	pwm= angle;
-	pwm = ((pwm/delta_angle)+1);
-	pwm = (pwm/(20))*Timer->ARR;
+}
 	
-	if (Voie == 1)
-	{Timer->CCR1 = (int)pwm;}
-	if (Voie == 2)
-	{Timer->CCR2 = (int)pwm;}
-	if (Voie == 3)
-	{Timer->CCR3 = (int)pwm;}
-	if (Voie == 4)
-	{Timer->CCR4 = (int)pwm;}	
-
-	/*aux = (angle*1)/180;
-	dutycycle = (1 + aux )/20;
-	Timer->ARR *(Duty_cycle*/
-
-
-	
-
-	return (int)pwm;
+float servo(float duty,TIM_TypeDef*Timer, char Voie)
+{
+	//PWM_init(Timer, Voie, 0.050);
+	Set_duty_cycle(Timer, Voie , duty);
+	return 0;
 }
